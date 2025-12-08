bc8d3c34f412d81533909f496aab13c0f9455f0e
Lucas Le <lucas.lpnl@byteuptech.com>
2025-12-08 10:41:04 +0800

    Practice UI

--- godot4/maze-escape/MainMenu.cs
+++ godot4/maze-escape/MainMenu.cs
@@ -1,20 +1,60 @@
 using Godot;
+using System;
 
-public partial class MainMenu : CanvasLayer
+public partial class MainMenu : Node
 {
-	public override void _Ready()
-	{
-		GetNode<Button>("MarginContainer/VBoxContainer/Start").Pressed += OnStartPressed;
-		GetNode<Button>("MarginContainer/VBoxContainer/Exit").Pressed += OnExitPressed;
-	}
+    [Export] public Button StartButton { get; set; }
+    [Export] public Button QuitButton { get; set; }
 
-	private void OnStartPressed()
-	{
-		GetTree().ChangeSceneToFile("res://scenes/Main.tscn");
-	}
+    public override void _Ready()
+    {
+        // Kiểm tra null trước khi subscribe
+        if (StartButton != null)
+            StartButton.Pressed += OnStartPressed;
+        else
+            GD.PrintErr("StartButton is not assigned!");
+            
+        if (QuitButton != null)
+            QuitButton.Pressed += OnQuitPressed;
+        else
+            GD.PrintErr("QuitButton is not assigned!");
+    }
 
-	private void OnExitPressed()
-	{
-		GetTree().Quit();
-	}
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("ui_accept"))
+        {
+            // Gọi pressed của nút đang focus
+            var focusOwner = GetViewport().GuiGetFocusOwner();
+            if (focusOwner is Button btn)
+            {
+                btn.EmitSignal(Button.SignalName.Pressed);
+            }
+        }
+    }
+
+    private void OnStartPressed()
+    {
+        // Unsubscribe trước khi chuyển scene để tránh memory leak
+        if (StartButton != null)
+            StartButton.Pressed -= OnStartPressed;
+        if (QuitButton != null)
+            QuitButton.Pressed -= OnQuitPressed;
+            
+        GetTree().ChangeSceneToFile("res://Main.tscn");
+    }
+
+    private void OnQuitPressed()
+    {
+        GetTree().Quit();
+    }
+    
+    // Clean up khi node bị xóa
+    public override void _ExitTree()
+    {
+        if (StartButton != null)
+            StartButton.Pressed -= OnStartPressed;
+        if (QuitButton != null)
+            QuitButton.Pressed -= OnQuitPressed;
+    }
 }
